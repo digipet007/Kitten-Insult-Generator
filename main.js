3894653740e13b8cf692dccd1e163175a35b48f8
Sarah Arnold <digipet007@gmail.com>
2019-11-18 14:46:59 -0800

    added firebase click tracker

--- main.js
+++ main.js
@@ -17,25 +17,40 @@ var database = firebase.database();
 
 // Begin Click counter logic ================================================
 var clickCounter;
+var timeDiff;
+var goAheadAndAjax = true;
 
 //access Firebase
 database.ref().on(
   "value",
   function(snapshot) {
     console.log(snapshot.val());
-    // Change the value of our clickCounter to match the value in the database
+    // Change the value of our clickCounter and timestamp to match the values in the database
     clickCounter = snapshot.val().clickcount;
-
-    // Console Log the value of the clickCounter
+    var timestamp1 = snapshot.val().timestamp;
     console.log("here is the click counter");
     console.log(clickCounter);
+    console.log("here is the timestamp");
+    console.log(timestamp1);
+    // Find the different between the database's timestamp and the current time
+    currTime = moment();
+    timeDiff = Math.abs(currTime.diff(timestamp1, "minutes"));
+    console.log("time Diff:");
+    console.log(timeDiff);
+    //if over 24 hours have passed, reset the clickcounter to 0, to be pushed into the database in the on click function
+    if (timeDiff > 1440) {
+      clickCounter = 0;
+    }
+    if (clickCounter > 300) {
+      goAheadAndAjax = false;
+    }
   },
   function(errorObject) {
     console.log("The read failed: " + errorObject.code);
   }
 );
 
-// below is all the Words API code ===========================================
+// below is all of the Words API code ===================================================================================
 
 // nounURL for generating nouns, adjURL for generating adjectives, noun for saving the noun, adj for saving the adjective
 var nounURL =
@@ -46,46 +61,59 @@ var adj;
 var noun;
 
 function randomWords() {
-  // get adjective, save in adj variable
-  $.ajax({
-    async: true,
-    crossDomain: true,
-    url: adjURL,
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-      "x-rapidapi-key": "39812ffa96mshad44da94a22ebb0p1f8c02jsn4c08e013d57d"
-    }
-  }).then(function(response) {
-    console.log(response);
-    adj = response.word;
-    $("#adjective-2").prepend(" " + adj);
+  if (goAheadAndAjax) {
+    // get adjective, save in adj variable
+    $.ajax({
+      async: true,
+      crossDomain: true,
+      url: adjURL,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": "39812ffa96mshad44da94a22ebb0p1f8c02jsn4c08e013d57d"
+      }
+    }).then(function(response) {
+      console.log(response);
+      adj = response.word;
+      $("#adjective-2").prepend(" " + adj);
 
-    updateDatabase();
-  });
+      updateDatabase();
+    });
 
-  // get noun, save in noun variable
-  $.ajax({
-    async: true,
-    crossDomain: true,
-    url: nounURL,
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-      "x-rapidapi-key": "39812ffa96mshad44da94a22ebb0p1f8c02jsn4c08e013d57d"
-    }
-  }).then(function(response) {
-    console.log(response);
-    noun = response.word;
-    $("#noun").prepend(" " + noun);
+    // get noun, save in noun variable
+    $.ajax({
+      async: true,
+      crossDomain: true,
+      url: nounURL,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": "39812ffa96mshad44da94a22ebb0p1f8c02jsn4c08e013d57d"
+      }
+    }).then(function(response) {
+      console.log(response);
+      noun = response.word;
+      $("#noun").prepend(" " + noun);
 
-    updateDatabase();
-  });
+      updateDatabase();
+    });
+  } else {
+    $("#noun").prepend(
+      "... <br><hr>Sorry! We have reached our API call limit! Come back tomorrow for more insults!"
+    );
+    return false;
+  }
 }
 
 // On click event updates clickcounter in database, and generates random words
 $("#generate").on("click", function() {
-  randomWords();
+  if (!goAheadAndAjax) {
+    console.log("too many click counts!!");
+    return false;
+  }
+  if (goAheadAndAjax) {
+    randomWords();
+  }
 });
 
 //update the database with the number of ajax calls
@@ -95,7 +123,7 @@ function updateDatabase() {
   // // Save new value to Firebase
   database
     .ref()
-    .set({
+    .update({
       clickcount: clickCounter
     })
     .then(function() {
@@ -106,6 +134,20 @@ function updateDatabase() {
       console.log("Synchronization failed");
     });
 
+  if (timeDiff >= 1440) {
+    database
+      .ref()
+      .update({
+        timestamp: firebase.database.ServerValue.TIMESTAMP
+      })
+      .then(function() {
+        console.log("Synchronization succeeded");
+        console.log(clickCounter);
+      })
+      .catch(function(error) {
+        console.log("Synchronization failed");
+      });
+  }
   // Log the value of clickCounter
   console.log(clickCounter);
 }
